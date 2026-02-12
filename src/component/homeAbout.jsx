2bb7f423f9c60b3536b556580aa8d1f7bc9df5c7
RohiniBalan <144508582+RohiniBalan@users.noreply.github.com>
2026-02-12 17:56:31 +0530

    Update homeAbout.jsx

--- src/component/homeAbout.jsx
+++ src/component/homeAbout.jsx
@@ -1,16 +1,24 @@
+import Heritage from "../assets/heritage.png";
+
 function HomeAbout(){
   return(
     <div className="home-about">
     <h1 className="text-center">Why HeritageLens ?</h1>
-      <div className="d-flex">
-        <div>
-      <p>HeritageLens is a digital platform designed to explore and 
+      <div className="row align-items-center justify-content-evenly">
+        <div className="col-lg-5 col-md-12">
+         <p>HeritageLens is a digital platform designed to explore and 
         preserve India’s cultural heritage. It provides organized, 
         interactive, and educational content that helps users 
         understand traditions, art forms, festivals, and architecture 
-        in a simple and engaging way.</p>
+        in a simple and engaging way.
+         </p>
+        </div>
+        <div>
+        <img src={Heritage} />
         </div>
     </div>
     </div>
   )
 }
+
+export default HomeAbout;

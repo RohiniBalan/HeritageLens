4b5a1e8c58ff03721c782b6eec02d2dc442895fb
RohiniBalan <144508582+RohiniBalan@users.noreply.github.com>
2026-02-12 17:22:10 +0530

    Create home about.jsx

---
+++ src/component/home about.jsx
@@ -0,0 +1,16 @@
+function HomeAbout(){
+  return(
+    <div className="home-about">
+    <h1 className="text-center">Why HeritageLens ?</h1>
+      <div className="d-flex">
+        <div>
+      <p>HeritageLens is a digital platform designed to explore and 
+        preserve India’s cultural heritage. It provides organized, 
+        interactive, and educational content that helps users 
+        understand traditions, art forms, festivals, and architecture 
+        in a simple and engaging way.</p>
+        </div>
+    </div>
+    </div>
+  )
+}
